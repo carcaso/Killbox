8fff4c95f5881350bc0494b520e1b122a3043c62
Filipe Lopes <flopescontas@gmail.com>
2019-10-08 23:29:34 +0100

    Implement key with collision

--- src/org/academiadecodigo/secondrow/killbox/CollisionDetector.java
+++ src/org/academiadecodigo/secondrow/killbox/CollisionDetector.java
@@ -23,20 +23,21 @@ public class CollisionDetector {
         boolean isBumpingRight = false;
         boolean isBumpingLeft = false;
 
+        //PLayerInformation
+        int playerStartX = player.getX();
+        int playerStartY = player.getY();
+        int playerEndX = player.getX() + Var.PLAYER_WIDTH;
+        int playerEndY = player.getY() + Var.PLAYER_HEIGHT;
+
         for (int i = 0; i < map.getPlatforms().length; i++) {
 
-            // Auxiliary variables to checkCollisionWithPlatforms collision
-            int playerStartX = player.getX();
-            int playerStartY = player.getY();
-            int playerEndX = player.getX() + Var.PLAYER_WIDTH;
-            int playerEndY = player.getY() + Var.PLAYER_HEIGHT;
-
+            //PLatform information
             int objectStartX = map.getPlatform(i).getX();
             int objectStartY = map.getPlatform(i).getY();
             int objectEndX = map.getPlatform(i).getX() + map.getPlatform(i).getWidth();
             int objectEndY = map.getPlatform(i).getY() + map.getPlatform(i).getHeight();
 
-
+            //parentesis a mais removido
             if ((playerEndX > objectStartX && playerStartX < objectEndX)) {
 
                 if (playerEndY == objectStartY) {
@@ -61,11 +62,47 @@ public class CollisionDetector {
             }
         }
 
+        /**
+         * Added the new method checkColision with key
+         * */
+        for(int i=0; i< map.getKeys().length; i++){
+
+
+            // Size of the key
+            int keyStartX = map.getKey(i).getX();
+            int keyStartY = map.getKey(i).getY();
+            int keyEndX = map.getKey(i).getX()+ map.getKey(i).getWidth();
+            int keyEndY = map.getKey(i).getY()+map.getKey(i).getHeigth();
+
+            if (playerEndX > keyStartX && playerStartX < keyEndX){
+
+                if(playerEndY == keyStartY){
+                    map.getKey(i).setCollide();
+                }
+
+                if(playerStartY == keyEndY){
+                    map.getKey(i).setCollide();
+                }
+            }
+
+            // Checks if player is in between a platform and sees if height is the same.
+            if ((playerStartY >= keyStartY && playerStartY <= keyEndY)
+                    || (playerEndY >= keyStartY && playerEndY <= keyEndY)) {
+                if (playerEndX == keyStartX) {
+                    map.getKey(i).setCollide();
+                }
+
+                if (playerStartX == keyEndX) {
+                    map.getKey(i).setCollide();
+                }
+            }
+
+        }
+
         boolean[] ret = {isBumpingHead, isLanding, isBumpingRight, isBumpingLeft};
         return ret;
     }
 
-    // TODO: 08/10/2019 collision with key
 
     // TODO: 08/10/2019 collision with door
 }
